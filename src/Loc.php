857906d0157a586e9a7900d66368d491e7bea392
Siarhei Buinevich <a5-kosmos@yandex.ru>
2025-07-16 14:55:43 +0300

    init

---
+++ src/Loc.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kosmos\Bitrix\Localization;
+
+use Bitrix\Main\Localization\Loc as BaseLoc;
+use Bitrix\Main\Config\Configuration;
+
+class Loc
+{
+    protected static ?string $defaultLanguage = null;
+
+    public function __call($name, $arguments)
+    {
+        return call_user_func_array([BaseLoc::class, $name], $arguments);
+    }
+
+    public static function __callStatic($name, $arguments)
+    {
+        return call_user_func_array([BaseLoc::class, $name], $arguments);
+    }
+
+    public static function getMessage(
+        string $code,
+        ?array $replace = null,
+        ?string $language = null,
+        ?string $defaultLanguage = null
+    ): ?string {
+        $message = BaseLoc::getMessage($code, $replace, $language);
+        if ($message === null) {
+            $defaultLanguage ??= static::getDefaultLanguage();
+            $message = BaseLoc::getMessage($code, $replace, $defaultLanguage);
+        }
+
+        return $message;
+    }
+
+    protected static function getDefaultLanguage(): ?string
+    {
+        if (static::$defaultLanguage === null) {
+            $config = Configuration::getValue('localization');
+            static::$defaultLanguage = $config['defaultLanguage'] ?: 'ru';
+        }
+
+        return static::$defaultLanguage;
+    }
+}
